b7bd7426ecfac88d861ccc1275bf176a24c9aa05
kaushaljnk27 <kaushaljnk2227@gmail.com>
2025-08-12 12:19:20 -0400

    logging and exception

--- setup.py
+++ setup.py
@@ -9,20 +9,20 @@ def get_requirements(file_path:str)->List[str]:
 
     requirements=[]
     with open(file_path) as file_obj:
-        requirements=file_obj.readline()
-        # requirements = [req.replace("\n", "") for req in requirements]
+    
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
-    return requirements
+    return requirements        
 
 setup(
-name ='mlproject', 
-vesion='0.0.1',
-author='Kaushal',
-author_email='kaushaljnk2227@gmail.com',
-packages=find_packages(),
-install_requires=get_requirements('requirements.txt')
-
-
+    name='mlproject',
+    version='0.0.1',
+    author='Kaushal',
+    author_email='kaushaljnk2227@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
 )
